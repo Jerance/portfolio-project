3c13180c06d7b5d8f3722db914dda9e6440e3341
Jerance MICHAEL <jerancemichael@MacBook-Pro-de-admin-7.local>
2021-11-21 16:12:13 +0100

    Ajout de la NavBar + Header et début de about-me

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import React from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Portfolio_container/Home/Navbar";
+import Header from "./Portfolio_container/Home/Header";
+import Footer from "./Portfolio_container/Home/Footer/Footer";
+import AboutMe from "./Portfolio_container/Home/About-me/about-me";
+import Particles from "react-particles-js";
+
 
 function App() {
   return (
-      <h2>Test</h2>
+      <div>
+          <Particles
+              params={{
+                  particles: {
+                      number: {
+                          value:25,
+                          density: {
+                              enable: true,
+                              value_area: 900,
+                          },
+                      },
+                      shape:{
+                          type:"star",
+                          stroke:{
+                              width:6,
+                              color:"#009BFF"
+                          },
+                      },
+                  },
+                  interactivity: { //**
+                      "detect_on": "window"
+                  },
+
+              }}
+          />
+          <Navbar/>
+          <Header/>
+          <AboutMe/>
+          <Footer/>
+      </div>
   );
 }
 
---
+++ src/Portfolio_container/Home/About-me/about-me.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './style-about.css';
+
+const AboutMe =() => {
+
+    return(
+        <div id="about"className="about_container">
+
+            <h1> BONJOUR 0 TOUS </h1>
+            <h1> BONJOUR 0 TOUS </h1><h1> BONJOUR 0 TOUS </h1><h1> BONJOUR 0 TOUS </h1>
+            <h1> BONJOUR 0 TOUS </h1>
+            <h1> BONJOUR 0 TOUS </h1><h1> BONJOUR 0 TOUS </h1>
+
+
+
+
+
+        </div>
+
+    )}
+
+export default AboutMe;
